954f9043df396dbcf703c2c996cc0f0509f5832f
amitkvaio <46754383+amitkvaio@users.noreply.github.com>
2025-09-27 09:28:05 +0530

    Updating the code content.
    Updating the code content.

--- CodeWithAmit_Basic/src/BreakAndContinue.java
+++ CodeWithAmit_Basic/src/BreakAndContinue.java
@@ -1,30 +1,22 @@
 public class BreakAndContinue {
 	public static void main(String[] args) {
-		System.out.println("########### Break Statement ###############");
-		for (int i = 1; i <= 5; i++) {
-			if (i == 3) {
-				break; // stop the loop when i = 3
-			}
-			System.out.println(i);
-		}
 
-		System.out.println("########### Continue Statement ###############");
+		System.out.println("########### Break Statement ##############");
 		for (int i = 1; i <= 5; i++) {
 			if (i == 3) {
-				continue; // skip when i = 3
-			}
-			System.out.println(i);
-		}
-		
-		System.out.println("#### Break and Continue Statement ####");
-		for (int i = 0; i < 6; i++) {
-			if (i == 2) {
-				continue;
-			}
-			if (i == 4) {
+				System.out.println("Key found in drawer no" + i);
 				break;
 			}
-			System.out.println(i);
+			System.out.println("Drawer no" + i + " → key Not found");
 		}
+		
+		System.out.println("########### Continue Statement ##############");
+		for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                System.out.println("Student " + i + " is absent. Skipping...");
+                continue;
+            }
+            System.out.println("Student " + i + " is present.");
+        }
 	}
 }
